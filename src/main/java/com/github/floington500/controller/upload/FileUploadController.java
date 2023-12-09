beda0b43c36bbab40f2e2c973f00feba0cb86e68
floington500 <zachappella@gmail.com>
2023-12-09 10:04:25 -0500

    feat: add controllers for providing access to endpoints for uploading and downloading

---
+++ src/main/java/com/github/floington500/controller/upload/FileUploadController.java
@@ -0,0 +1,36 @@
+package com.github.floington500.controller.upload;
+
+import com.github.floington500.controller.service.FileService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/upload")
+@RequiredArgsConstructor
+public class FileUploadController {
+
+    private final FileService fileService;
+
+    @GetMapping("/status")
+    public ResponseEntity<String> getStatus() {
+        return ResponseEntity.ok("OK");
+    }
+
+    /**
+     * Endpoint for handling requests from the client to upload files.
+     *
+     * @param file the file uploaded by the client
+     * @param request used to map the file the client wants to the local filesystem.
+     * @return if the upload was successful
+     */
+    @PostMapping("**")
+    public ResponseEntity<String> uploadFile(
+            @RequestParam("file") MultipartFile file,
+            HttpServletRequest request
+    ) {
+        return fileService.uploadFile(file, request.getRequestURI());
+    }
+}
