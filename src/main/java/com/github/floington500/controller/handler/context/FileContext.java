3cdb3355235ac14d34d0731270d7961912520e58
floington500 <zachappella@gmail.com>
2023-12-09 15:26:07 -0500

    fix: remove `@Getter` which is not supported on records

--- src/main/java/com/github/floington500/controller/handler/context/FileContext.java
+++ src/main/java/com/github/floington500/controller/handler/context/FileContext.java
@@ -1,6 +1,5 @@
 package com.github.floington500.controller.handler.context;
 
-import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -9,6 +8,5 @@ import org.springframework.web.multipart.MultipartFile;
  * @param payload the data for the file
  * @param URI the URL that the user submitted
  */
-@Getter
 public record FileContext(MultipartFile payload, String URI) {
 }
