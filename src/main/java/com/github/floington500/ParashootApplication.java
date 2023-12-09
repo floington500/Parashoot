c5cac06cb0e82ca5aa719d0501ea66f663a58410
floington500 <zachappella@gmail.com>
2023-12-09 09:57:24 -0500

    initial commit

---
+++ src/main/java/com/github/floington500/ParashootApplication.java
@@ -0,0 +1,13 @@
+package com.github.floington500;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParashootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParashootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/floington500/ParashootApplicationTests.java
@@ -0,0 +1,7 @@
+package com.github.floington500;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParashootApplicationTests {
+}
