424b7e0d2c5f903651c1069037ef9767c2f377f6
floington500 <zachappella@gmail.com>
2024-01-14 00:19:00 -0500

    feat: improve how local path is resolved

--- src/main/java/com/github/floington500/common/command/FileOperation.java
+++ src/main/java/com/github/floington500/common/command/FileOperation.java
@@ -1,8 +1,12 @@
 package com.github.floington500.common.command;
 
 import com.github.floington500.common.command.context.FileContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Provides a base implementation for handlers.
@@ -13,38 +17,50 @@ import org.springframework.web.multipart.MultipartFile;
 public abstract class FileOperation implements Operation {
 
     /**
+     *
      * Prefixed for paths provided by the request to map them to the local filesystem path.
      */
-    private static final String VAR = "/var";
+    @Value("${server.local.storage}")
+    private String PATH;
+
     protected String filename;
     protected String directoryPath;
 
     @Override
     public ResponseEntity<Object> handle(FileContext ctx) {
-        filename = toLocalPath("/upload") + pathToFilename(ctx.URI());
-        directoryPath = toLocalPath(ctx.URI());
+        if (ctx.payload().isEmpty()) {
+            filename = extractFilenameFromURI(ctx.URI());
+        } else {
+            filename = ctx.payload().getOriginalFilename();
+        }
 
-        return handleFile(ctx.payload());
+        directoryPath = constructDirectoryPath(ctx.URI());
+
+        return handleFile(ctx);
     }
 
     /**
      * Extracts the filename from a path.
      */
-    private String pathToFilename(String path) {
-        // TODO: handle case of user querying root
-        return path.split("/files")[1];
+    private String extractFilenameFromURI(String path) {
+        return Paths.get(path).getFileName().toString();
     }
 
-
     /**
-     * Maps external paths from the request to one for the local filesystem.
+     * Maps external paths from the request to one for
+     * the local filesystem.
      *
      * @param path the external path from the request
      * @return a path that can be used for the local filesystem
      */
-    private String toLocalPath(String path) {
-        return VAR + path;
+    private String constructDirectoryPath(String path) {
+        Path varPath = Paths.get(PATH);
+        if (path.indexOf("files") != -1) {
+            Path relativePath = Paths.get(path.substring(7));
+            return varPath.resolve(relativePath).getParent().toString();
+        }
+        return varPath.resolve(path.substring(8)).toString();
     }
 
-    protected abstract ResponseEntity<Object> handleFile(MultipartFile payload);
+    protected abstract ResponseEntity<Object> handleFile(FileContext ctx);
 }
